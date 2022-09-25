1a9e230e8db52331d25a3355772b1cce3e98f3d6
Đoàn Đức Mạnh <ducmanhchy@gmail.com>
2022-09-26 00:10:30 +0700

    update

--- BlogSystem.Web/Controllers/HomeController.cs
+++ BlogSystem.Web/Controllers/HomeController.cs
@@ -7,12 +7,17 @@ using BlogSystem.Data.Models;
 using BlogSystem.Data.Repositories;
 using BlogSystem.Web.ViewModels.Home;
 using BlogSystem.Web.ViewModels.Posts;
+using System.Web.Caching;
+using System.Collections.Generic;
 
 namespace BlogSystem.Web.Controllers
 {
     public class HomeController : BaseController
     {
         private readonly IDbRepository<Post> postsData;
+        private string ConfigHideADHomeControler = ConfigurationManager.AppSettings["ConfigHideADHomeControler"].ToString();
+        private string[] ConfigShowADHomeControler = ConfigurationManager.AppSettings["ConfigShowADHomeControler"].ToString().Split(',');
+
 
         public HomeController(IDbRepository<Post> postsData)
         {
@@ -32,11 +37,26 @@ namespace BlogSystem.Web.Controllers
         //DANG SU DUNG
         public ActionResult Index_Theme3()
         {
-            var postsPage = this.postsData.All().Where(x => x.ParentType == "HOME" && x.isPublish == true).OrderByDescending(p => p.CreatedOn);
-            var posts = this.Mapper.Map<PostViewModel>(postsPage).ToList();
-
-            var blogsPage = this.postsData.All().Where(x => x.ParentType == "BL" && x.isPublish == true).OrderByDescending(p => p.CreatedOn);
-            var blogs = this.Mapper.Map<PostViewModel>(blogsPage).ToList();
+            List<PostViewModel> posts = new List<PostViewModel>();
+            List<PostViewModel> blogs = new List<PostViewModel>();
+            Cache ca = new Cache();
+            if (ca["CACHE_HOME_POST"] != null && ca["CACHE_HOME_POST"] != null)
+                posts = (List<PostViewModel>)ca["CACHE_HOME_POST"];
+            else
+            {
+                var postsPage = this.postsData.All().Where(x => x.ParentType == "HOME" && x.status != -1 && x.isPublish == true).OrderByDescending(p => p.CreatedOn);
+                posts = this.Mapper.Map<PostViewModel>(postsPage).ToList();
+                ca.Insert("CACHE_HOME_POST", posts, null, DateTime.MaxValue, TimeSpan.FromHours(6));
+            }
+            
+            if (ca["CACHE_HOME_BLOG"] != null && ca["CACHE_HOME_BLOG"] != null)
+                blogs = (List<PostViewModel>)ca["CACHE_HOME_BLOG"];
+            else
+            {
+                var blogsPage = this.postsData.All().Where(x => x.ParentType == "BL" && x.status != -1 && x.isPublish == true).OrderByDescending(p => p.CreatedOn);
+                blogs = this.Mapper.Map<PostViewModel>(blogsPage).ToList();
+                ca.Insert("CACHE_HOME_BLOG", blogs, null, DateTime.MaxValue, TimeSpan.FromHours(6));
+            }
 
             var model = new IndexHomeViewModel
             {
